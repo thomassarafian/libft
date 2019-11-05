37bfd7d59ad51f321b74f0e9da49c4b1b84d8cbb
Thomas SARAFIAN <tsarafia@e3r3p16.42.fr>
2019-11-05 19:46:01 +0100

    ft_strnstr.c

--- test.c
+++ test.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
-int main()
-{
-    printf("atoi('gladir')=%i\n",atoi("gladir"));
-    printf("atoi('gladir.')=%i\n",atoi("gladir."));
-    printf("atoi('gladir.com')=%i\n",atoi("gladir.com"));
-    printf("atoi('16gladir.com')=%i\n",atoi("16gladir.com"));
-    printf("atoi('gladir.com16')=%i\n",atoi("gladir.com16"));
-    printf("atoi('gladir16.17com')=%i\n",atoi(" 2147483648"));
-    return 0;
+#include <string.h>
+int main() {
+  char S[] ="Ceci est une chaine de caracteres avec Gladir.com contenu dedans !";
+  printf("%s\n",strnstr(S,"Gladir.com", 48));
+  return 0;
 }
