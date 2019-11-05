3801716cf1a1210cef7e6a0103677f297fc63b43
Thomas SARAFIAN <tsarafia@e3r3p16.42.fr>
2019-11-05 16:59:05 +0100

    ft_isprint OK

--- ft_isprint.c
+++ ft_isprint.c
@@ -6,11 +6,11 @@
 /*   By: tsarafia <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/05 12:09:43 by tsarafia          #+#    #+#             */
-/*   Updated: 2019/11/05 12:12:32 by tsarafia         ###   ########.fr       */
+/*   Updated: 2019/11/05 16:58:56 by tsarafia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_isprint(int c)
+int	ft_isprint(int c)
 {
 	if (c >= 32 && c <= 126)
 		return (1);
