35b62d1a2f9a81453f10e505e5d7d86f2964a8ab
Thomas SARAFIAN <tsarafia@e3r3p16.42.fr>
2019-11-05 16:54:16 +0100

    ft_isdigit OK

--- test.c
+++ test.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <ctype.h>
 
-char the_chars[] = { 'A', 0x80, 'Z' };
-
-#define SIZE sizeof( the_chars ) / sizeof( char )
-
-int main( void )
-{
-    int i;
-
-    for( i = 0; i < SIZE; i++ ) {
-        if( isascii( the_chars[i] ) ) {
-            printf( "Char %c is an ASCII character\n",
-                the_chars[i] );
-        } else {
-            printf( "Char %c is not an ASCII character\n",
-                the_chars[i] );
-        }
-    }
+int main () {
+   int var1 = 'h';
+   int var2 = '2';
     
-    return EXIT_SUCCESS;
+   if( isdigit(var1) ) {
+      printf("var1 = |%c| is a digit\n", var1 );
+   } else {
+      printf("var1 = |%c| is not a digit\n", var1 );
+   }
+   
+   if( isdigit(var2) ) {
+      printf("var2 = |%c| is a digit\n", var2 );
+   } else {
+      printf("var2 = |%c| is not a digit\n", var2 );
+   }
+   
+   return(0);
 }
