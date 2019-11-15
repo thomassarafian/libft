8185e881dbb0be01fa6777d6dc6d5b258dea21bc
Thomas SARAFIAN <tsarafia@e1r5p18.42.fr>
2019-11-15 16:08:58 +0100

    norme

--- ft_lstdelone_bonus.c
+++ ft_lstdelone_bonus.c
@@ -6,7 +6,7 @@
 /*   By: tsarafia <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/13 19:38:17 by tsarafia          #+#    #+#             */
-/*   Updated: 2019/11/15 15:40:08 by tsarafia         ###   ########.fr       */
+/*   Updated: 2019/11/15 16:06:31 by tsarafia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ void	ft_lstdelone(t_list *lst, void (*del)(void*))
 {
 	if (!lst || !del)
 		return ;
-
 	del(lst->content);
 	free(lst);
 }
